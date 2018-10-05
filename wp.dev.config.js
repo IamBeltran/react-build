be6278c0a9029db38d3b443fa313a1c9eccc10c0
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2018-10-05 00:58:20 -0500

    Se crean archivos de webpack, para diferentes entornos de desarrollo

---
+++ wp.dev.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = { 
+//	ENABLED USEFUL TOOLS FOR DEVELOPMENT
+    mode: "development",
+    watch: true,
+
+}
---
+++ wp.prd.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = {
+//  ENABLE MANY OPTIMIZATIONS FOR PRODUCTION BUILDS
+    mode: "production",
+
+}
