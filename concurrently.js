f99ba93cf5caf7d83cccf902a0b456de06249c67
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2018-10-06 14:20:05 -0500

    Se mejora el script para el uso de concurrently

--- concurrently.js
+++ concurrently.js
@@ -1,19 +1,67 @@
+	"use strict";
+//	┌───────────────────────────────────────────────────────────────────────────────────┐
+//	│	IMPORT NODE MODULE DEPENDENCIES.												│
+//	└───────────────────────────────────────────────────────────────────────────────────┘
+	const concurrently	= require('concurrently');
+	const colors		= require('colors');
 
-	const concurrently = require('concurrently');
-	
-	function success(){console.log(':)')};
-	function failure(){console.log(':(')};
-	
-	concurrently([
-		'npm:build-*', 
-			{
-				command: 'nodemon',
-				name: 'DEVELOPMENT',
-				//prefixColor:'green'
-			}
-		],
+//	┌───────────────────────────────────────────────────────────────────────────────────┐
+//	│	DECLARATION OF CONSTANTS.														│
+//	└───────────────────────────────────────────────────────────────────────────────────┘
+
+//	──[ NODE ENVIRONMENT.	]────────────────────────────────────────────────────────────
+	const ENV = process.env.NODE_ENV;
+	const TASK_RUN = process.env.TASK;
+
+//	──[ CONMMANDS.	]────────────────────────────────────────────────────────────────────
+	const conmmands = [
+		// 0
 		{
-			prefix: 'name',
-			killOthers: ['failure', 'success'],
-			restartTries: 3,
-		}).then(success, failure);
+			command:	'json-server -c ./jsonserver/json-server.json ./jsonserver/json-server-data.json',
+			name:		'JSV',
+			prefixColor:'green'
+		},
+		// 1
+		{
+			command:	'webpack-dev-server',
+			name:		'WDS',
+			prefixColor:'blue'
+		},
+	];
+
+//	──[ CONMMANDS.	]────────────────────────────────────────────────────────────────────	
+	const TASK = {
+		REACT_DEV: [
+			conmmands[0],
+			conmmands[1]
+		]
+	};
+
+	const OPTIONS = {
+		prefix: 			'name',
+		prefixLength:		10,
+		killOthers:			['failure', 'success'],
+		successCondition:	'all',
+		restartDelay:		5000,
+		restartTries: 		3
+	}
+
+//	┌───────────────────────────────────────────────────────────────────────────────────┐
+//	│	RUN CONCURRENTLY.																│
+//	└───────────────────────────────────────────────────────────────────────────────────┘	
+	concurrently(
+		TASK["REACT_DEV"],
+		OPTIONS
+	)
+	.then(
+		() => {  
+			console.log(`────────────────────────────────────────────────────`.green);
+			console.log(`[SUCCESS] → SE COMPLETARON LAS TAREAS o(^-^)o`.green)
+			console.log(`────────────────────────────────────────────────────`.green);
+		},	
+		err => {
+			console.log(`────────────────────────────────────────────────────`.red);
+			console.log(`[FAILURE] → NO SE COMPLETARON LAS TAREAS (つ﹏<。)`.red)
+			console.log(`────────────────────────────────────────────────────`.red);
+		}	
+	);
